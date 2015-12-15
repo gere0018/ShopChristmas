3bc912a330ea4d83763850b3b767d7f2b2f8c970
Nehmat <gere0018@algonquinlive.com>
2015-12-15 12:55:10 -0500

    Added clear button inside input search html, but ng-click is not working still, there is an issue in ionic regarding this.

--- www/js/search/searchController.js
+++ www/js/search/searchController.js
@@ -4,12 +4,15 @@ angular.module('ShopChristmas')
     $scope.nehmat = {
         userInput : ""
     }
+    $scope.clearInput = function(){
+        $scope.nehmat.userInput = "";
+        console.log("badri");
+    }
     $scope.getData = function(){
       console.log("inside getData function");
       SearchData.getSearchData($scope.nehmat.userInput);
       //needed to add a timeout as it takes some time to get back the search results
       $timeout(function(){
-        console.log("badri");
         console.log(SearchData.getResults().data.products);
         $scope.searches = SearchData.getResults().data.products;
         $ionicSlideBoxDelegate.update();
