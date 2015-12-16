b634fea6184d029060da0641118fae5a07f12ce9
Nehmat <gere0018@algonquinlive.com>
2015-12-15 20:42:22 -0500

    moved the ionic loading functionality to the search service instead of controller. Handled the case of nor products that match search with ionic pop up

--- www/js/search/searchController.js
+++ www/js/search/searchController.js
@@ -1,29 +1,20 @@
 angular.module('ShopChristmas')
 .controller('SearchCtrl', ['$scope','$state', 'SearchData', '$timeout',
- '$ionicSlideBoxDelegate','$ionicLoading',
-    function($scope, $state, SearchData, $timeout, $ionicSlideBoxDelegate,$ionicLoading){
+ '$ionicSlideBoxDelegate',
+    function($scope, $state, SearchData, $timeout, $ionicSlideBoxDelegate){
     $scope.nehmat = {
         userInput : ""
     }
     $scope.clearInput = function(){
         $scope.nehmat.userInput = "";
-        console.log("badri");
     }
     $scope.getData = function(){
       console.log("inside getData function");
       SearchData.getSearchData($scope.nehmat.userInput);
-       //show loading icon while we get the data
-      $ionicLoading.show({
-        template: '<ion-spinner icon = "ios" class= "spinner-assertive"></ion-spinner>'
-      });
-
-    
       //needed to add a timeout as it takes some time to get back the search results
       $timeout(function(){
         console.log(SearchData.getResults().data.products);
-        $scope.searches = SearchData.getResults().data.products
-        //hide loading icon
-        $ionicLoading.hide();
+        $scope.searches = SearchData.getResults().data.products        
         $ionicSlideBoxDelegate.update();
       }, 1000);
 
--- www/js/search/searchService.js
+++ www/js/search/searchService.js
@@ -1,20 +1,39 @@
 angular.module('ShopChristmas')
-.factory('SearchData', ['$http', function($http) {
+.factory('SearchData', ['$http', '$ionicLoading', '$ionicPopup', function($http,$ionicLoading, $ionicPopup) {
   var searchResults;
   return{
     getSearchData: function(userInput){
+      //show loading icon while we get the data
+      $ionicLoading.show({
+        template: '<ion-spinner icon = "ios" class= "spinner-assertive"></ion-spinner>'
+      });
+
       $http.get("http://api.bestbuy.com/v1/products((search="
         + userInput + 
         "))?show=name,sku,salePrice,image&format=json&apiKey=5w2n4mecqg8awc9sbba26gps")
-        .then(function(data){
-          console.log(" this is the " + userInput);
-             searchResults = data;                       
+        .then(function(searchData){
+          console.log(searchData); 
+          //handle Scenario where we don't get any search result
+          if(searchData.data.products.length == 0){
+            //hide loading icon
+            $ionicLoading.hide();
+            $ionicPopup.alert({
+                  title: "No items found!!!",
+                  content: "There are currently no items that match you search!! Please try again."
+                });
+            searchResults = searchData;
+          }else{
+            searchResults = searchData;               
+          }
+                                 
           }, function(error){
           console.log(error);
           });
         },
     getResults: function(){
-      return searchResults;
+      //hide loading icon
+      $ionicLoading.hide();
+      return searchResults;       
     }
 
   };
